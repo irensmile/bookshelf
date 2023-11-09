1e7419d83785c06c50e254d40422148fe21e9a1f
SvetaSaletska <svitlanagalevich@gmail.com>
2023-11-10 01:37:24 +0200

    Update shopping.js

--- src/js/shoppinglist.js
+++ src/js/shoppinglist.js
@@ -1,6 +1,31 @@
 
 const elements = {
-container: document.querySelector('.js-list')
+container: document.querySelector('.js-list'),
+totalQuantity: document.querySelector('.js-quantity'),
+clearBtn: document.querySelector('.js-close')
+}
+const BOOKS_KEY_KEY = 'shoppinglist'
+const books = JSON.parse(localStorage.getItem(BOOKS_KEY)) ?? [];
+
+elements.container.insertAdjacentHTML('afterbegin', createMarkup(books));
+
+console.log(books);
+function createMarkup(arr) {
+  return arr.map(({img, titlle, category, desc, author})=> `
+  <li class='card-item' class='book-img'>
+  <img src='${img}' alt ='${titlle}'>
+  <h2>${titlle}</h2>
+  <p>${category}</p>
+  <h3>${desc}</h3>
+  <p>${author}</p>
+</li>`).join('');
 }
 
-const products = JSON.parse(localStorage.getItem);
+if (books.length){
+  const quantity = books.reduce((acc, {qty}) => acc + qty, 0);
+  elements.totalQuantity.textContent = quantity;
+  elements.container.insertAdjacentHTML('afterbegin', createMarkup(books));
+  } else {
+    elements.totalQuantity.textContent = 'This page is empty, add some books and proceed to order.'
+
+  }
