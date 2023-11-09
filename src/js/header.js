f6456b3aaa8ebe3f8ccba797d7d786ceed8546b6
Maria <98313650+MariaMasyk@users.noreply.github.com>
2023-11-09 21:34:26 +0100

    write code-header

---
+++ src/js/header.js
@@ -0,0 +1,24 @@
+//burger-js
+const ToggleEl = document.querySelector('.box-toggle');
+const mobMenuEl = document.querySelector('.mob-menu');
+const boxMobMenuEl = document.querySelector('.box-mob-menu');
+
+ToggleEl.addEventListener('click', onClickToggle);
+function onClickToggle(event) {
+  event.currentTarget.classList.toggle('active');
+}
+
+mobMenuEl.addEventListener('click', onClickMobMenu);
+function onClickMobMenu(event) {
+  event.currentTarget.classList.toggle('active');
+  boxMobMenuEl.classList.toggle('active');
+  
+  const body = document.querySelector('body');
+  if (boxMobMenuEl.classList.contains('active')) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
+}
+
+document.querySelector('#bestsellers').classList.add('active');
