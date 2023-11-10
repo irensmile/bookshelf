714fc0f7ed378a2ca96fb7ca332051d6559e4d0b
irensmile <curkan.ira@gmail.com>
2023-11-10 03:57:48 +0000

    Deploying to gh-pages from @ irensmile/project-ScriptMasters14@d46ee12228af1145899871cce99ede8f18622679 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,10 @@
-import"./assets/header-89b59c6d.js";document.querySelector(".js-list");JSON.parse(localStorage.getItem);
+import"./assets/header-89b59c6d.js";const t={container:document.querySelector(".js-list"),totalQuantity:document.querySelector(".js-quantity"),clearBtn:document.querySelector(".js-close"),pageName:document.querySelector(".js-title")},c="shoppinglist",e=JSON.parse(localStorage.getItem(c))??[];t.container.insertAdjacentHTML("afterbegin",s(e));console.log(e);function s(n){return n.map(({img:a,titlle:o,category:r,desc:l,author:i})=>`
+<li class='card-item' class='book-img'>
+  <img src='${a}' alt ='${o}'>
+  <h2>${o}</h2>
+  <p>${r}</p>
+  <h3>${l}</h3>
+  <p>${i}</p>
+</li>
+<button type="button" class="js-close">Close</button>`).join("")}if(e.length){const n=e.reduce((a,{qty:o})=>a+o,0);t.totalQuantity.textContent=n,t.container.insertAdjacentHTML("afterbegin",s(e)),t.clearBtn.addEventListener(click,u)}else t.totalQuantity.textContent="This page is empty, add some books and proceed to order.";function u(){localStorage.removeItem(c)}
 //# sourceMappingURL=commonHelpers2.js.map
