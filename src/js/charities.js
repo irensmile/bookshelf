01686df2b60f5136fb7e741038347d897c33e138
SvetaSaletska <svitlanagalevich@gmail.com>
2023-11-11 20:15:20 +0200

    update

--- src/js/charities.js
+++ src/js/charities.js
@@ -1,61 +1,61 @@
-import { sup_org } from "./organizations";
+// import { sup_org } from "./organizations";
 
-const refs = {
-    list: document.querySelector('.org-list'),
-    btn: document.querySelector('.btn-up'),
-    icon: document.querySelector('.icon-arrow')
-}
+// const refs = {
+//     list: document.querySelector('.org-list'),
+//     btn: document.querySelector('.btn-up'),
+//     icon: document.querySelector('.icon-arrow')
+// }
 
-let number = 1;
-let currentItemIdx = 0;
-const maxItems = sup_org.length;
-let itemHeight = 0;
+// let number = 1;
+// let currentItemIdx = 0;
+// const maxItems = sup_org.length;
+// let itemHeight = 0;
 
-function markupItems() {
-  sup_org.forEach(item => {
-    const listItem = `
-      <li>
-        <a
-        href="${item.url}"
-        target="_blank"
-        class="org-item"
-        >
-          0${number++}
-          <img
-          srcset="${item.img} 1x, ${item.img2} 2x"
-          src="${item.img}" alt="${item.title}"
-          class="org-img"
-           />
-        </a>
-      </li>
-    `;
-    refs.list.insertAdjacentHTML('beforeend', listItem);
-  });
-    itemHeight = refs.list.firstElementChild.clientHeight;
-};
+// function markupItems() {
+//   sup_org.forEach(item => {
+//     const listItem = `
+//       <li>
+//         <a
+//         href="${item.url}"
+//         target="_blank"
+//         class="org-item"
+//         >
+//           0${number++}
+//           <img
+//           srcset="${item.img} 1x, ${item.img2} 2x"
+//           src="${item.img}" alt="${item.title}"
+//           class="org-img"
+//            />
+//         </a>
+//       </li>
+//     `;
+//     refs.list.insertAdjacentHTML('beforeend', listItem);
+//   });
+//     itemHeight = refs.list.firstElementChild.clientHeight;
+// };
 
-refs.btn.addEventListener('click', onScrollBtnClick);
+// refs.btn.addEventListener('click', onScrollBtnClick);
 
-function onScrollBtnClick() {
-    let itms = 8;
-    currentItemIdx = currentItemIdx + itms;
+// function onScrollBtnClick() {
+//     let itms = 8;
+//     currentItemIdx = currentItemIdx + itms;
 
-    if (currentItemIdx >= maxItems) {
-        currentItemIdx = 0;
-        refs.list.scrollTo({
-            top: currentItemIdx * itemHeight,
-            behavior: 'smooth',
-        });
-      refs.icon.classList.remove('transform');
-      refs.btn.classList.remove('active-btn');
-    } else {
-        refs.list.scrollTo({
-            top: currentItemIdx * itemHeight,
-            behavior: 'smooth',
-        });
-      refs.icon.classList.add('transform');
-      refs.btn.classList.add('active-btn');
-    }
-}
+//     if (currentItemIdx >= maxItems) {
+//         currentItemIdx = 0;
+//         refs.list.scrollTo({
+//             top: currentItemIdx * itemHeight,
+//             behavior: 'smooth',
+//         });
+//       refs.icon.classList.remove('transform');
+//       refs.btn.classList.remove('active-btn');
+//     } else {
+//         refs.list.scrollTo({
+//             top: currentItemIdx * itemHeight,
+//             behavior: 'smooth',
+//         });
+//       refs.icon.classList.add('transform');
+//       refs.btn.classList.add('active-btn');
+//     }
+// }
 
-markupItems()
+// markupItems()
--- src/js/shoppinglist.js
+++ src/js/shoppinglist.js
@@ -60,118 +60,118 @@ const bookslist =
       ],
       "__v": 0
     },
-    // {
-    //   "_id": "642fd89ac8cf5ee957f122db",
-    //   "list_name": "Paperback Nonfiction",
-    //   "date": "2023-04-07T08:46:57.000Z",
-    //   "age_group": "",
-    //   "amazon_product_url": "https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20",
-    //   "article_chapter_link": "",
-    //   "author": "Michelle Zauner",
-    //   "book_image": "https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg",
-    //   "book_image_width": 338,
-    //   "book_image_height": 500,
-    //   "book_review_link": "",
-    //   "book_uri": "nyt://book/520cb070-10ce-5d43-9b0c-e77f57d84a8d",
-    //   "contributor": "by Michelle Zauner",
-    //   "contributor_note": "",
-    //   "created_date": "2023-04-05 22:05:27",
-    //   "description": "The daughter of a Korean mother and Jewish American father, and leader of the indie rock project Japanese Breakfast, describes creating her own identity after losing her mother to cancer.",
-    //   "first_chapter_link": "",
-    //   "price": "0.00",
-    //   "primary_isbn10": "1984898957",
-    //   "primary_isbn13": "9781984898951",
-    //   "publisher": "Vintage",
-    //   "rank": 2,
-    //   "rank_last_week": 0,
-    //   "sunday_review_link": "",
-    //   "title": "CRYING IN H MART",
-    //   "updated_date": "2023-04-05 22:10:17",
-    //   "weeks_on_list": 1,
-    //   "buy_links": [
-    //     {
-    //       "name": "Amazon",
-    //       "url": "https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20"
-    //     },
-    //     {
-    //       "name": "Apple Books",
-    //       "url": "https://goto.applebooks.apple/9781984898951?at=10lIEQ"
-    //     },
-    //     {
-    //       "name": "Barnes and Noble",
-    //       "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781984898951"
-    //     },
-    //     {
-    //       "name": "Books-A-Million",
-    //       "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FCRYING%252BIN%252BH%252BMART%252FMichelle%252BZauner%252F9781984898951&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DCRYING%252BIN%252BH%252BMART%252BMichelle%252BZauner"
-    //     },
-    //     {
-    //       "name": "Bookshop",
-    //       "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781984898951&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DCRYING%2BIN%2BH%2BMART"
-    //     },
-    //     {
-    //       "name": "IndieBound",
-    //       "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781984898951%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DCRYING%2BIN%2BH%2BMART%2BMichelle%2BZauner%26aff%3DNYT"
-    //     }
-    //   ],
-    //   "__v": 0
-    // },
-    // {
-    //   "_id": "642fd89ac8cf5ee957f122de",
-    //   "list_name": "Paperback Nonfiction",
-    //   "date": "2023-04-07T08:46:57.000Z",
-    //   "age_group": "",
-    //   "amazon_product_url": "http://www.amazon.com/Thinking-Fast-Slow-Daniel-Kahneman-ebook/dp/B00555X8OA?tag=NYTBSREV-20",
-    //   "article_chapter_link": "",
-    //   "author": "Daniel Kahneman",
-    //   "book_image": "https://storage.googleapis.com/du-prd/books/images/9781429969352.jpg",
-    //   "book_image_width": 330,
-    //   "book_image_height": 490,
-    //   "book_review_link": "https://www.nytimes.com/2011/11/27/books/review/thinking-fast-and-slow-by-daniel-kahneman-book-review.html",
-    //   "book_uri": "nyt://book/1e1ffccd-d526-5154-84de-1628b31454e9",
-    //   "contributor": "by Daniel Kahneman",
-    //   "contributor_note": "",
-    //   "created_date": "2023-04-05 22:05:27",
-    //   "description": "When we can and cannot trust our intuitions in making business and personal decisions.",
-    //   "first_chapter_link": "",
-    //   "price": "0.00",
-    //   "primary_isbn10": "0374533555",
-    //   "primary_isbn13": "9780374533557",
-    //   "publisher": "Farrar, Straus & Giroux",
-    //   "rank": 5,
-    //   "rank_last_week": 4,
-    //   "sunday_review_link": "https://www.nytimes.com/2011/11/27/books/review/thinking-fast-and-slow-by-daniel-kahneman-book-review.html",
-    //   "title": "THINKING, FAST AND SLOW",
-    //   "updated_date": "2023-04-05 22:10:17",
-    //   "weeks_on_list": 356,
-    //   "buy_links": [
-    //     {
-    //       "name": "Amazon",
-    //       "url": "http://www.amazon.com/Thinking-Fast-Slow-Daniel-Kahneman-ebook/dp/B00555X8OA?tag=NYTBSREV-20"
-    //     },
-    //     {
-    //       "name": "Apple Books",
-    //       "url": "https://goto.applebooks.apple/9780374533557?at=10lIEQ"
-    //     },
-    //     {
-    //       "name": "Barnes and Noble",
-    //       "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9780374533557"
-    //     },
-    //     {
-    //       "name": "Books-A-Million",
-    //       "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FTHINKING%25252C%252BFAST%252BAND%252BSLOW%252FDaniel%252BKahneman%252F9780374533557&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DTHINKING%25252C%252BFAST%252BAND%252BSLOW%252BDaniel%252BKahneman"
-    //     },
-    //     {
-    //       "name": "Bookshop",
-    //       "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9780374533557&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DTHINKING%252C%2BFAST%2BAND%2BSLOW"
-    //     },
-    //     {
-    //       "name": "IndieBound",
-    //       "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9780374533557%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DTHINKING%252C%2BFAST%2BAND%2BSLOW%2BDaniel%2BKahneman%26aff%3DNYT"
-    //     }
-    //   ],
-    //   "__v": 0
-    // },
+    {
+      "_id": "642fd89ac8cf5ee957f122db",
+      "list_name": "Paperback Nonfiction",
+      "date": "2023-04-07T08:46:57.000Z",
+      "age_group": "",
+      "amazon_product_url": "https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20",
+      "article_chapter_link": "",
+      "author": "Michelle Zauner",
+      "book_image": "https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg",
+      "book_image_width": 338,
+      "book_image_height": 500,
+      "book_review_link": "",
+      "book_uri": "nyt://book/520cb070-10ce-5d43-9b0c-e77f57d84a8d",
+      "contributor": "by Michelle Zauner",
+      "contributor_note": "",
+      "created_date": "2023-04-05 22:05:27",
+      "description": "The daughter of a Korean mother and Jewish American father, and leader of the indie rock project Japanese Breakfast, describes creating her own identity after losing her mother to cancer.",
+      "first_chapter_link": "",
+      "price": "0.00",
+      "primary_isbn10": "1984898957",
+      "primary_isbn13": "9781984898951",
+      "publisher": "Vintage",
+      "rank": 2,
+      "rank_last_week": 0,
+      "sunday_review_link": "",
+      "title": "CRYING IN H MART",
+      "updated_date": "2023-04-05 22:10:17",
+      "weeks_on_list": 1,
+      "buy_links": [
+        {
+          "name": "Amazon",
+          "url": "https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20"
+        },
+        {
+          "name": "Apple Books",
+          "url": "https://goto.applebooks.apple/9781984898951?at=10lIEQ"
+        },
+        {
+          "name": "Barnes and Noble",
+          "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781984898951"
+        },
+        {
+          "name": "Books-A-Million",
+          "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FCRYING%252BIN%252BH%252BMART%252FMichelle%252BZauner%252F9781984898951&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DCRYING%252BIN%252BH%252BMART%252BMichelle%252BZauner"
+        },
+        {
+          "name": "Bookshop",
+          "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781984898951&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DCRYING%2BIN%2BH%2BMART"
+        },
+        {
+          "name": "IndieBound",
+          "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781984898951%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DCRYING%2BIN%2BH%2BMART%2BMichelle%2BZauner%26aff%3DNYT"
+        }
+      ],
+      "__v": 0
+    },
+    {
+      "_id": "642fd89ac8cf5ee957f122de",
+      "list_name": "Paperback Nonfiction",
+      "date": "2023-04-07T08:46:57.000Z",
+      "age_group": "",
+      "amazon_product_url": "http://www.amazon.com/Thinking-Fast-Slow-Daniel-Kahneman-ebook/dp/B00555X8OA?tag=NYTBSREV-20",
+      "article_chapter_link": "",
+      "author": "Daniel Kahneman",
+      "book_image": "https://storage.googleapis.com/du-prd/books/images/9781429969352.jpg",
+      "book_image_width": 330,
+      "book_image_height": 490,
+      "book_review_link": "https://www.nytimes.com/2011/11/27/books/review/thinking-fast-and-slow-by-daniel-kahneman-book-review.html",
+      "book_uri": "nyt://book/1e1ffccd-d526-5154-84de-1628b31454e9",
+      "contributor": "by Daniel Kahneman",
+      "contributor_note": "",
+      "created_date": "2023-04-05 22:05:27",
+      "description": "When we can and cannot trust our intuitions in making business and personal decisions.",
+      "first_chapter_link": "",
+      "price": "0.00",
+      "primary_isbn10": "0374533555",
+      "primary_isbn13": "9780374533557",
+      "publisher": "Farrar, Straus & Giroux",
+      "rank": 5,
+      "rank_last_week": 4,
+      "sunday_review_link": "https://www.nytimes.com/2011/11/27/books/review/thinking-fast-and-slow-by-daniel-kahneman-book-review.html",
+      "title": "THINKING, FAST AND SLOW",
+      "updated_date": "2023-04-05 22:10:17",
+      "weeks_on_list": 356,
+      "buy_links": [
+        {
+          "name": "Amazon",
+          "url": "http://www.amazon.com/Thinking-Fast-Slow-Daniel-Kahneman-ebook/dp/B00555X8OA?tag=NYTBSREV-20"
+        },
+        {
+          "name": "Apple Books",
+          "url": "https://goto.applebooks.apple/9780374533557?at=10lIEQ"
+        },
+        {
+          "name": "Barnes and Noble",
+          "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9780374533557"
+        },
+        {
+          "name": "Books-A-Million",
+          "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FTHINKING%25252C%252BFAST%252BAND%252BSLOW%252FDaniel%252BKahneman%252F9780374533557&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DTHINKING%25252C%252BFAST%252BAND%252BSLOW%252BDaniel%252BKahneman"
+        },
+        {
+          "name": "Bookshop",
+          "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9780374533557&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DTHINKING%252C%2BFAST%2BAND%2BSLOW"
+        },
+        {
+          "name": "IndieBound",
+          "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9780374533557%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DTHINKING%252C%2BFAST%2BAND%2BSLOW%2BDaniel%2BKahneman%26aff%3DNYT"
+        }
+      ],
+      "__v": 0
+    },
     // {
     //   "_id": "642fd89ac8cf5ee957f122df",
     //   "list_name": "Paperback Nonfiction",
@@ -869,7 +869,7 @@ function createMarkup(arr) {
   <h2 class='shopping_title'>${title}</h2>
   <p class='list_name'>${list_name}</p>
   <h3 class='desc overflow-ellipsis'>${description}</h3>
-  <p class= 'shopping_author>'${author}'</p>
+  <p class= 'shopping_author'>${author}</p>
   </div>
   <button type="button" class="js-close btn-delete">
   <svg class="icon-delete" width="28" height="28">
@@ -903,7 +903,7 @@ function createMarkup(arr) {
        </svg>
      </a>
  </li>
-<ul/>
+</ul>
 </li>`).join('');
 
 }
