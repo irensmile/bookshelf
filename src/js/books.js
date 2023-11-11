5e3610540b27bc892f2e30669ec85866170d60db
irensmile <curkan.ira@gmail.com>
2023-11-11 00:07:44 -0500

    books template

--- src/js/books.js
+++ src/js/books.js
@@ -11,16 +11,32 @@ const list = document.querySelector('.books')
 
 function fetchBooks(data) {
 
-const markup = data.map((res) => {
-          return `<ul class="books-list"> 
-                  <li> 
-                     <img src="${res.book_image}" class="book-pic" />
-                     <p class="book-name">${res.title}</p>
-                      <p class="book-author">${res.author}</p>
-                  </li>
-                 </ul>`
+    const markup = data.map((res) => {
+        console.log('->', res);
+        return `<div>${res.list_name}</div>
+                <ul>
+                    ${booksMarkup(res.books)}
+                </ul>`
+        return `<ul class='category_books'></ul>`
+        return `<ul class="books-list"> 
+                <li> 
+                    <img src="${res.book_image}" class="book-pic" />
+                    <p class="book-name">${res.title}</p>
+                    <p class="book-author">${res.author}</p>
+                </li>
+                </ul>`
       });
       list.insertAdjacentHTML('beforeend', markup)
       console.log(list)
+}
+
+function booksMarkup(books) {
+    return books.map((book) => {
+        return `<li>
+            <img src="${book.book_image}" class="book-pic" />
+            <p class="book-name">${book.title}</p>
+            <p class="book-author">${book.author}</p>
+        </li>`
+    }).join('');
 }
   
