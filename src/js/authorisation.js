6d52c145946d0c72e7b2d4f47e913adbde382751
svitlana-horodylova <horodylova.sv@gmail.com>
2023-11-13 23:38:28 +0000

    button toggle added

--- src/js/authorisation.js
+++ src/js/authorisation.js
@@ -1,48 +1,68 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const openModalBtn = document.getElementById('sign-in-button-main');
-    const authModal = document.getElementById('authModal');
-    const authFormClose = document.querySelector('.auth-form-close');
-    const signupLink = document.getElementById('signup-link');
-    const signinLink = document.getElementById('signin-link');
-    const signupForm = document.getElementById('signup-form');
-    const signinForm = document.getElementById('signin-form');
-  
-    openModalBtn.addEventListener('click', function () {
-      authModal.style.display = 'block';
-    });
-  
-    authFormClose.addEventListener('click', function () {
-      authModal.style.display = 'none';
-    });
-  
-    signupLink.addEventListener('click', function () {
-      signinForm.style.display = 'none';
-      signupForm.style.display = 'block';
-    });
-  
-    signinLink.addEventListener('click', function () {
-      signupForm.style.display = 'none';
-      signinForm.style.display = 'block';
-    });
-  
-    signupForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-  
-      const userData = {
-        name,
-        email,
-        password,
-      };
-  
-      const userDataJSON = JSON.stringify(userData);
-  
-      localStorage.setItem('userData', userDataJSON);
-      this.reset();
-    });
+  const authModal = document.getElementById('authModal');
+  const openModalBtn = document.getElementById('sign-in-button-main');
+  const authFormClose = document.querySelector('.auth-form-close');
+  const signupLink = document.getElementById('signup-link');
+  const signinLink = document.getElementById('signin-link');
+  const signupForm = document.getElementById('signup-form');
+  const signinForm = document.getElementById('signin-form');
+  const submitButton = document.querySelector('.btn-sign-up');
+
+  openModalBtn.addEventListener('click', function () {
+    openAuthModal(authModal);
   });
-  
-   
+
+  authFormClose.addEventListener('click', function () {
+    authModal.style.display = 'none';
+  });
+
+  signupLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    updateButtonText('Sign up');
+    signupForm.style.display = 'block';
+    signinForm.style.display = 'none';
+  });
+
+  signinLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    updateButtonText('Sign in');
+    signinForm.style.display = 'block';
+    signupForm.style.display = 'none';
+  });
+
+  signupForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const userData = {
+      name,
+      email,
+      password,
+    };
+
+    saveUserData(userData);
+
+    authModal.style.display = 'none';
+
+    this.reset();
+  });
+
+  function openAuthModal(modal) {
+    modal.style.display = 'block';
+  }
+
+  function saveUserData(userData) {
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+    existingUsers.push(userData);
+    localStorage.setItem('users', JSON.stringify(existingUsers));
+  }
+
+  function updateButtonText(action) {
+    if (submitButton) {
+      submitButton.textContent = action;
+    }
+  }
+});
