0714d20607927020608cd7959cb4098a12cc96cf
irensmile <curkan.ira@gmail.com>
2023-11-12 21:55:47 -0500

    added missing file

---
+++ src/js/books-api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.baseURL = 'https://books-backend.p.goit.global/';
+
+export async function getTopBooks() {
+    const response = await axios.get('/books/top-books');
+    return response.data
+}
+
+export async function getBooksByCategory(category) {
+    const response = await axios.get(`books/category?category=${category}`);
+    return response.data
+}
