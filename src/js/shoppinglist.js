9b7d87c79f910359f1112535ccb764d055b84721
irensmile <curkan.ira@gmail.com>
2023-11-14 17:24:23 -0500

    Dynamic sellers list

--- src/js/shoppinglist.js
+++ src/js/shoppinglist.js
@@ -17,46 +17,27 @@ elements.container.addEventListener('click', (event) => {
 })
 
 function createMarkup(arr) {
-  return arr.map(({_id, book_image, title, list_name, description, author})=> `
+  return arr.map(({_id, book_image, title, list_name, description, author, buy_links})=> `
 <li data-id='${_id}' class='card-item'>
-  <img src='${book_image}' alt ='${title}' class='img'>
-  <div class="info">
-  <h2 class='shopping_title'>${title}</h2>
-  <p class='list_name'>${list_name}</p>
-  <h3 class='desc overflow-ellipsis'>${description}</h3>
-  <p class= 'shopping_author'>${author}</p>
-  </div>
+    <img src='${book_image}' alt ='${title}' class='img'>
+    <div class="info">
+      <h2 class='shopping_title'>${title}</h2>
+      <p class='list_name'>${list_name}</p>
+      <h3 class='desc overflow-ellipsis'>${description}</h3>
+      <div class='info-author-socials'>
+        <p class= 'shopping_author'>${author}</p>
+        <ul class="socials_list">
+          ${createBuyLinksMarkup(buy_links)}
+        </ul>
+      </div>
+    </div>
+
   <button type="button" class="js-clear btn-delete", data-id=${_id}>
-  <svg class="icon-delete" width="28" height="28">
-  <use href="./images/icons.svg#icon-trash"></use>
-  </svg>
+    <svg class="icon-delete" width="28" height="28">
+      <use href="./images/icons.svg#icon-trash"></use>
+    </svg>
   </button>
-   <ul class="socials_list">
-   <li class = "amazon_logo">
-     <a
-       class="social-icon-amazon"
-       href="https://www.amazon.com/"
-       target="_blank"
-       rel="nofollow noopener noreferrer"
-       aria-label="Amazon icon"
-     >
-     <img src="./images/amazon_logo.jpg" alt="amazon" width ='48px' height='15' />
-
-     </a>
-   </li>
-   <li class = "book-icon">
-     <a
-       class="social-icon-book"
-       href="https://www.apple.com/ua/apple-books/"
-       target="_blank"
-     rel="nofollow noopener noreferree"
-       aria-label="Book icon"
-     >
-     <img src="./images/apple_books.jpg" alt="books" width ='32px' height='32px'/>
-
-     </a>
- </li>
-</ul>
+  
 </li>`).join('');
 }
 
@@ -64,50 +45,51 @@ function createBuyLinksMarkup(buyLinks) {
     return buyLinks.map((buyLink) => {
       let sellerLogo = null;
       let cssClass = null;
-    switch (buyLink.name) {
-      case 'Amazon': {
-        cssClass = "social-icon-amazon"
-        sellerLogo = '../images/amazon_logo.jpg';
-        break;
+      console.log("!!!", buyLink.name)
+      switch (buyLink.name) {
+        case 'Amazon': {
+          cssClass = "social-icon-amazon"
+          console.log('****')
+          sellerLogo = '../images/amazon_logo.jpg';
+          break;
+        }
+        case 'Apple Books': {
+          sellerLogo = '../images/apple_books.jpg';
+          cssClass = "social-icon-book";
+          break;
+        }
+        // case 'Barnes and Nobel': {
+        //   sellerLogo = '';
+        //   break;
+        // }
+        // case 'Books-A-Million': {
+        //   sellerLogo = '';
+        //   break;
+        // }
+        // case 'BookShop': {
+        //   sellerLogo = '';
+        //   break;
+        // }
+        // case 'Indie Bound': {
+        //   sellerLogo = '';
+        //   break;
+        // }
+        default: {
+          sellerLogo = '../images/book.jpg';
+          cssClass = "social-icon-book";
+        }
       }
-      case 'Apple Books': {
-        sellerLogo = '../images/apple_books.jpg';
-        cssClass = "social-icon-book";
-        break;
-      }
-      // case 'Barnes and Nobel': {
-      //   sellerLogo = '';
-      //   break;
-      // }
-      // case 'Books-A-Million': {
-      //   sellerLogo = '';
-      //   break;
-      // }
-      // case 'BookShop': {
-      //   sellerLogo = '';
-      //   break;
-      // }
-      // case 'Indie Bound': {
-      //   sellerLogo = '';
-      //   break;
-      // }
-      default: {
-        sellerLogo = '../images/book.jpg';
-        cssClass = "social-icon-book";
-      }
-    }
-    return `<li class = "book-icon">
-     <a
-       class="social-icon-book"
-       href=${buyLink.url}
-       target="_blank"
-     rel="nofollow noopener noreferree"
-       aria-label="Book icon"
-     >
-     <img src=${sellerLogo} alt="${buyLink.name}" width ='32px' height='32px'/>
-
-     </a>
- </li>`}
+      return `<li class = "book-icon">
+      <a
+        class=${cssClass}
+        href=${buyLink.url}
+        target="_blank"
+      rel="nofollow noopener noreferree"
+        aria-label="Book icon"
+      >
+      <img src=${sellerLogo} alt="${buyLink.name}" width ='32px' height='32px'/>
+      </a>
+  </li>`}
   ).join('');
 }
 
