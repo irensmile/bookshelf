6bb5167f67a6575233db7c57d64b4b1e22fb50a5
irensmile <curkan.ira@gmail.com>
2023-11-13 19:57:39 -0500

    1. Clean up some unused files. Add a stub for popup.html, as it is raising an error

--- src/js/popup.js
+++ src/js/popup.js
@@ -13,23 +13,20 @@ const elements = {
   removeBtn: document.querySelector('.remove'),
   openListBtn: document.querySelector('.open-list'),
   modalText: document.querySelector('.modal-add-text'),
+  booksListElement: document.querySelector('.books')
 };
 
-const booksListElement = document.querySelector('.books');
-if (booksListElement) {
-  // Цей код не буде працювати на shopping html, оскільки там немає Div з класом book
-  // потрібно доробити, щоб показувати модалку і на shopping.html
-  booksListElement.addEventListener('click', event => {
-    let cardBookId = null;
-    if (event.target.classList.contains('book-block'))
-      cardBookId = event.target.dataset.book;
-    else if (event.target.parentNode.classList.contains('book-block'))
-      cardBookId = event.target.parentNode.dataset.book;
-    else return;
+elements.booksListElement.addEventListener('click', event => {
+  let cardBookId = null;
+  if (event.target.classList.contains('book-block'))
+    cardBookId = event.target.dataset.book;
+  else if (event.target.parentNode.classList.contains('book-block'))
+    cardBookId = event.target.parentNode.dataset.book;
+  else return;
 
-    getBookInfo(cardBookId);
-  });
-}
+  getBookInfo(cardBookId);
+});
+
 
 async function getBookInfo(id) {
   const BASE_URL = 'https://books-backend.p.goit.global/books/';
