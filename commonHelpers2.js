8496098686cf6cabde666fd4f73ce96e1194b572
irensmile <curkan.ira@gmail.com>
2023-11-15 21:45:40 +0000

    Deploying to gh-pages from @ irensmile/project-ScriptMasters14@7c93427351866644dc848f6369e781c6f90c30a8 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import"./assets/scroll-up-2f72e7b4.js";const a={container:document.querySelector(".js-list"),totalQuantity:document.querySelector(".js-quantity"),clearBtn:document.querySelector(".js-clear"),pageName:document.querySelector(".js-title"),message:document.querySelector(".js-message"),booksImg:document.querySelector(".js-books")},l="booksInList",i=JSON.parse(localStorage.getItem(l))??[];a.container.addEventListener("click",s=>{s.target.classList.contains("icon-delete")&&u(s.target.parentNode.dataset.id)});function m(s){return s.map(({_id:e,book_image:o,title:t,list_name:n,description:c,author:r,buy_links:g})=>`
+import"./assets/scroll-up-2f72e7b4.js";const s={container:document.querySelector(".js-list"),totalQuantity:document.querySelector(".js-quantity"),clearBtn:document.querySelector(".js-clear"),pageName:document.querySelector(".js-title"),message:document.querySelector(".js-message"),booksImg:document.querySelector(".js-books")},i="booksInList";var l=null;n();s.container.addEventListener("click",o=>{o.target.classList.contains("icon-delete")&&d(o.target.parentNode.dataset.id)});function p(o){return o.map(({_id:e,book_image:t,title:a,list_name:c,description:r,author:g,buy_links:m})=>`
 <li data-id='${e}' class='card-item'>
-    <img src='${o}' alt ='${t}' class='img'>
+    <img src='${t}' alt ='${a}' class='img'>
     <div class="info">
-      <h2 class='shopping_title'>${t}</h2>
-      <p class='list_name'>${n}</p>
-      <h3 class='desc overflow-ellipsis'>${c}</h3>
+      <h2 class='shopping_title'>${a}</h2>
+      <p class='list_name'>${c}</p>
+      <h3 class='desc overflow-ellipsis'>${r}</h3>
       <div class='info-author-socials'>
-        <p class= 'shopping_author'>${r}</p>
+        <p class= 'shopping_author'>${g}</p>
         <ul class="socials_list">
-          ${d(g)}
+          ${u(m)}
         </ul>
       </div>
     </div>
@@ -19,15 +19,15 @@ import"./assets/scroll-up-2f72e7b4.js";const a={container:document.querySelector
     </svg>
   </button>
   
-</li>`).join("")}function d(s){return s.slice(0,4).map(e=>{let o=null,t=null;switch(e.name){case"Amazon":{t="social-icon-amazon",o="../images/amazon_logo.jpg";break}case"Apple Books":{o="../images/apple_books.jpg",t="social-icon-book";break}default:o="../images/book.jpg",t="social-icon-book"}return`<li class = "book-icon">
+</li>`).join("")}function u(o){return o.slice(0,4).map(e=>{let t=null,a=null;switch(e.name){case"Amazon":{a="social-icon-amazon",t="../images/amazon_logo.jpg";break}case"Apple Books":{t="../images/apple_books.jpg",a="social-icon-book";break}default:t="../images/book.jpg",a="social-icon-book"}return`<li class = "book-icon">
       <a
-        class=${t}
+        class=${a}
         href=${e.url}
         target="_blank"
       rel="nofollow noopener noreferree"
         aria-label="Book icon"
       >
-      <img src=${o} alt="${e.name}" width ='32px' height='32px'/>
+      <img src=${t} alt="${e.name}" width ='32px' height='32px'/>
       </a>
-  </li>`}).join("")}i.length?(a.booksImg.style.display="none",a.message.style.display="none",a.container.insertAdjacentHTML("afterbegin",m(i))):(a.booksImg.style.visibility="visible",a.message.style.visibility="visible");function u(s){const e=i.filter(o=>o._id!==s);e.length==0?localStorage.removeItem(l):localStorage.setItem(l,JSON.stringify(e)),window.location.href="./index.html"}
+  </li>`}).join("")}function n(){l=JSON.parse(localStorage.getItem(i))??[],l.length?(s.booksImg.style.display="none",s.message.style.display="none",s.container.innerHTML=p(l)):(s.container.innerHTML="",s.booksImg.style.display="block",s.message.style.display="block",s.booksImg.style.visibility="visible",s.message.style.visibility="visible")}function d(o){const e=l.filter(t=>t._id!==o);e.length==0?localStorage.removeItem(i):localStorage.setItem(i,JSON.stringify(e)),n()}
 //# sourceMappingURL=commonHelpers2.js.map
