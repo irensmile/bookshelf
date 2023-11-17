8c090b6b0d65c2dc5d10765e8b3eb565f4ca5235
irensmile <curkan.ira@gmail.com>
2023-11-17 13:56:37 +0000

    Deploying to gh-pages from @ irensmile/project-ScriptMasters14@092bae004e623aef5a758fa7ae31bf1436706d91 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,6 +1,6 @@
-import{a as d,b as m}from"./assets/scroll-up-d75a4643.js";const u="/project-ScriptMasters14/assets/icons-3746b069.svg",h="/project-ScriptMasters14/assets/books1-x-com-daafd4eb.jpg",s={container:document.querySelector(".js-list"),totalQuantity:document.querySelector(".js-quantity"),clearBtn:document.querySelector(".js-clear"),pageName:document.querySelector(".js-title"),message:document.querySelector(".js-message"),booksImg:document.querySelector(".js-books")},l="booksInList";var i=null;n();s.container.addEventListener("click",o=>{o.target.classList.contains("icon-delete")&&y(o.target.parentNode.dataset.id)});function b(o){return o.map(({_id:e,book_image:t,title:a,list_name:c,description:r,author:p,buy_links:g})=>`
+import{a as d,b as u}from"./assets/scroll-up-d75a4643.js";const m="/project-ScriptMasters14/assets/icons-3746b069.svg",b="/project-ScriptMasters14/assets/books1-x-com-daafd4eb.jpg",t={container:document.querySelector(".js-list"),totalQuantity:document.querySelector(".js-quantity"),clearBtn:document.querySelector(".js-clear"),pageName:document.querySelector(".js-title"),message:document.querySelector(".js-message"),booksImg:document.querySelector(".js-books")},l="booksInList";var i=null;n();t.container.addEventListener("click",s=>{var e=null;s.target.classList.contains("icon-delete")?e=s.target.parentNode:s.target.parentNode.classList.contains("icon-delete")?e=s.target.parentNode.parentNode:s.target.classList.contains("btn-delete")&&(e=s.target),e&&y(e.dataset.id)});function h(s){return s.map(({_id:e,book_image:o,title:a,list_name:c,description:r,author:p,buy_links:g})=>`
 <li data-id='${e}' class='shopping-card-item'>
-    <img src='${t}' alt ='${a}' class='shopping-img'>
+    <img src='${o}' alt ='${a}' class='shopping-img'>
     <div class="shopping-info">
       <h2 class='shopping_title description-title'>${a}</h2>
       <p class='shopping-list_name'>${c}</p>
@@ -15,11 +15,11 @@ import{a as d,b as m}from"./assets/scroll-up-d75a4643.js";const u="/project-Scri
 
   <button type="button" class="js-clear btn-delete", data-id=${e}>
     <svg class="icon-delete" width="28" height="28">
-      <use href="${u}#icon-trash"></use>
+      <use href="${m}#icon-trash"></use>
     </svg>
   </button>
 
-</li>`).join("")}function k(o){return o.slice(0,2).map(e=>{let t=null,a=null;switch(e.name){case"Amazon":{a="social-icon-amazon",t=`${m}`;break}case"Apple Books":{t=`${d}`,a="social-icon-book";break}default:t=`${h}`,a="social-icon-book"}return`<li class = "book-icon">
+</li>`).join("")}function k(s){return s.slice(0,2).map(e=>{let o=null,a=null;switch(e.name){case"Amazon":{a="social-icon-amazon",o=`${u}`;break}case"Apple Books":{o=`${d}`,a="social-icon-book";break}default:o=`${b}`,a="social-icon-book"}return`<li class = "book-icon">
       <a
         class=${a}
         href=${e.url}
@@ -27,7 +27,7 @@ import{a as d,b as m}from"./assets/scroll-up-d75a4643.js";const u="/project-Scri
       rel="nofollow noopener noreferree"
         aria-label="Book icon"
       >
-      <img src=${t} alt="${e.name}" width ='32px' height='32px'/>
+      <img src=${o} alt="${e.name}" width ='32px' height='32px'/>
       </a>
-  </li>`}).join("")}function n(){i=JSON.parse(localStorage.getItem(l))??[],i.length?(s.booksImg.style.display="none",s.message.style.display="none",s.container.innerHTML=b(i)):(s.container.innerHTML="",s.booksImg.style.display="block",s.message.style.display="block",s.booksImg.style.visibility="visible",s.message.style.visibility="visible")}function y(o){const e=i.filter(t=>t._id!==o);e.length==0?localStorage.removeItem(l):localStorage.setItem(l,JSON.stringify(e)),n()}
+  </li>`}).join("")}function n(){i=JSON.parse(localStorage.getItem(l))??[],i.length?(t.booksImg.style.display="none",t.message.style.display="none",t.container.innerHTML=h(i)):(t.container.innerHTML="",t.booksImg.style.display="block",t.message.style.display="block",t.booksImg.style.visibility="visible",t.message.style.visibility="visible")}function y(s){const e=i.filter(o=>o._id!==s);e.length==0?localStorage.removeItem(l):localStorage.setItem(l,JSON.stringify(e)),n()}
 //# sourceMappingURL=commonHelpers2.js.map
