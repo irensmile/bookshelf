368480512817b78ba03af20f9fb38df6b8db1a39
irensmile <curkan.ira@gmail.com>
2023-11-17 02:45:25 +0000

    Deploying to gh-pages from @ irensmile/project-ScriptMasters14@210d42a2ece0981a9c8999aa3075b2957bcaf895 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import"./assets/scroll-up-f97e5f49.js";const s={container:document.querySelector(".js-list"),totalQuantity:document.querySelector(".js-quantity"),clearBtn:document.querySelector(".js-clear"),pageName:document.querySelector(".js-title"),message:document.querySelector(".js-message"),booksImg:document.querySelector(".js-books")},i="booksInList";var l=null;n();s.container.addEventListener("click",o=>{o.target.classList.contains("icon-delete")&&d(o.target.parentNode.dataset.id)});function p(o){return o.map(({_id:e,book_image:t,title:a,list_name:c,description:r,author:g,buy_links:m})=>`
-<li data-id='${e}' class='card-item'>
-    <img src='${t}' alt ='${a}' class='img'>
-    <div class="info">
-      <h2 class='shopping_title'>${a}</h2>
-      <p class='list_name'>${c}</p>
-      <h3 class='desc overflow-ellipsis'>${r}</h3>
-      <div class='info-author-socials'>
-        <p class= 'shopping_author'>${g}</p>
+import"./assets/scroll-up-f97e5f49.js";const s={container:document.querySelector(".js-list"),totalQuantity:document.querySelector(".js-quantity"),clearBtn:document.querySelector(".js-clear"),pageName:document.querySelector(".js-title"),message:document.querySelector(".js-message"),booksImg:document.querySelector(".js-books")},l="booksInList";var a=null;n();s.container.addEventListener("click",o=>{o.target.classList.contains("icon-delete")&&d(o.target.parentNode.dataset.id)});function m(o){return o.map(({_id:e,book_image:t,title:i,list_name:c,description:r,author:p,buy_links:g})=>`
+<li data-id='${e}' class='shopping-card-item'>
+    <img src='${t}' alt ='${i}' class='shopping-img'>
+    <div class="shopping-info">
+      <h2 class='shopping_title description-title'>${i}</h2>
+      <p class='shopping-list_name'>${c}</p>
+      <h3 class='shopping-text description'>${r}</h3>
+      <div class='shopping-socials'>
+        <p class= 'shopping_author'>${p}</p>
         <ul class="socials_list">
-          ${u(m)}
+          ${u(g)}
         </ul>
       </div>
     </div>
@@ -18,10 +18,10 @@ import"./assets/scroll-up-f97e5f49.js";const s={container:document.querySelector
       <use href="./images/icons.svg#icon-trash"></use>
     </svg>
   </button>
-  
-</li>`).join("")}function u(o){return o.slice(0,2).map(e=>{let t=null,a=null;switch(e.name){case"Amazon":{a="social-icon-amazon",t="../images/amazon_logo.jpg";break}case"Apple Books":{t="../images/apple_books.jpg",a="social-icon-book";break}default:t="../images/book.jpg",a="social-icon-book"}return`<li class = "book-icon">
+
+</li>`).join("")}function u(o){return o.slice(0,2).map(e=>{let t=null,i=null;switch(e.name){case"Amazon":{i="social-icon-amazon",t="../images/amazon_logo.svg";break}case"Apple Books":{t="../images/apple_books.svg",i="social-icon-book";break}default:t="../images/book.jpg",i="social-icon-book"}return`<li class = "book-icon">
       <a
-        class=${a}
+        class=${i}
         href=${e.url}
         target="_blank"
       rel="nofollow noopener noreferree"
@@ -29,5 +29,5 @@ import"./assets/scroll-up-f97e5f49.js";const s={container:document.querySelector
       >
       <img src=${t} alt="${e.name}" width ='32px' height='32px'/>
       </a>
-  </li>`}).join("")}function n(){l=JSON.parse(localStorage.getItem(i))??[],l.length?(s.booksImg.style.display="none",s.message.style.display="none",s.container.innerHTML=p(l)):(s.container.innerHTML="",s.booksImg.style.display="block",s.message.style.display="block",s.booksImg.style.visibility="visible",s.message.style.visibility="visible")}function d(o){const e=l.filter(t=>t._id!==o);e.length==0?localStorage.removeItem(i):localStorage.setItem(i,JSON.stringify(e)),n()}
+  </li>`}).join("")}function n(){a=JSON.parse(localStorage.getItem(l))??[],a.length?(s.booksImg.style.display="none",s.message.style.display="none",s.container.innerHTML=m(a)):(s.container.innerHTML="",s.booksImg.style.display="block",s.message.style.display="block",s.booksImg.style.visibility="visible",s.message.style.visibility="visible")}function d(o){const e=a.filter(t=>t._id!==o);e.length==0?localStorage.removeItem(l):localStorage.setItem(l,JSON.stringify(e)),n()}
 //# sourceMappingURL=commonHelpers2.js.map
